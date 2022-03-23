296c7e3e183faf8a06124403de763f81c510c807
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-03-23 23:11:00 +0100

    connect to database

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
-import { Provider } from "react-redux";
-import store from "./store/index";
-
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
-  
+  const cartItems = useSelector((state) => state.shop.cartItems);
+  const totalCart = useSelector((state) => state.shop.totalCart);
+  const data = { cartItems, totalCart };
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_KEY, {
+      method: "PUT",
+      body: JSON.stringify(data),
+    });
+  }, [data]);
   return (
-    <Provider store={store}>
-      <Layout>
-        <Cart />
-        <Products />
-      </Layout>
-    </Provider>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
